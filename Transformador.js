846cf16873ce9128af2404f7b06321a4dcc039f4
Taglianini <tomastagliani1234@gmail.com>
2024-06-13 10:43:10 -0300

    Enunciado

---
+++ Transformador.js
@@ -0,0 +1,7 @@
+// 1 Crear la función transformarFrase.
+// 2 Crear una copia del array frase con la primera letra de cada palabra en mayúscula utilizando el método map.
+// 3 En cada iteración de map, convertir la primera letra de la palabra a mayúscula y concatenarla  con el resto de la palabra en minúscula utilizando los métodos charAt y slice. Devolver cada palabra con la primera letra en mayúscula.
+// 4 Unir las palabras de fraseMayuscula en una sola cadena de texto, separadas por un espacio, utilizando el método join
+// 5 Devolver la variable oracion desde la función.
+// 6 Por último, se puede invocar la función transformarFrase con el array frase como argumento para obtener la oración con las palabras en mayúscula.
+
